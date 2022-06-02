14c6c1126f14f78a7165a00a7654376741d8eefb
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-06-02 09:36:14 +0530

    Time: 18 ms (38.86%), Space: 10.5 MB (95.40%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -7,6 +7,7 @@ public:
         for(int i = 0; i < matrix.size(); i++){
             for(int j = 0; j < matrix[0].size(); j++){
                 ans[j][i] = matrix[i][j];
+                // ans[i][j] = matrix[j][i];
             }
         }
         
