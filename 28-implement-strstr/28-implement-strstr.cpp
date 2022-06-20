b0bd2e50e979345f1e2110d1e8b060aebd299356
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-06-20 16:53:56 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (90.21%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        
-    int found = haystack.find(needle);
-    if(found == -1)
-       return -1;
-   return found;
+    int strStr(string haystack, string needle) {   
+        int found = haystack.find(needle);
+        if(found == -1)
+            return -1;
+        return found;
     }
 };
