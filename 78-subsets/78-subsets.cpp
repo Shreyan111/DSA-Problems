6b146d657369b69f6335f84d4b1b25d9b2e7d5ec
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-06-30 21:19:24 +0530

    Time: 8 ms (23.49%), Space: 7.4 MB (47.09%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,26 @@
 class Solution {
-public:
-    vector<vector<int>> ans;
-    vector<int> temp;
-    int len;
-        
-    vector<vector<int>> subsets(vector<int>& nums) {
-        len = nums.size(); //3
-        get(0, nums); 
-        return ans;
-    }
+    void subsets_rec(vector<int>& nums, int idx, vector<vector<int>>& result){
+          if(idx == nums.size() - 1){
+              result.push_back({});
+              result.push_back({nums[idx]});
+          }
+        else{
+              subsets_rec(nums, idx+1, result);
+              int n = result.size();
+              for(int i = 0 ; i < n ; ++i){
+                  vector<int> r = result[i];
+                  r.push_back(nums[idx]);
+                  result.push_back(r);
+              }
+          }
+      }
     
-    void get(int i, vector<int>& nums){
-        if(i == len){
-            ans.push_back(temp);
-            return;
-        }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        if(nums.empty()) return {{}};
+        vector<vector<int>> result;
+        subsets_rec(nums, 0, result);
         
-        get(i + 1, nums); 
-        temp.push_back(nums[i]);
-        get(i + 1, nums);
-        temp.pop_back();   
+        return result;
     }
 };
