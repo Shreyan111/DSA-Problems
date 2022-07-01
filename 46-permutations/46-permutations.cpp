149a16f51c8ec5c51d273b385b6574b4d1c56bc4
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-01 21:38:45 +0530

    Time: 3 ms (81.18%), Space: 7.7 MB (55.24%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,14 +1,39 @@
+// class Solution {
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         sort(nums.begin(),nums.end());
+//         do 
+//         {
+//             ans.push_back(nums);
+//         }
+//         while(next_permutation(nums.begin(),nums.end()));
+        
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        do 
+    void permutation(vector<int> &nums, int index, vector<vector<int>>&ans)
+    {
+        if(index >= nums.size())
         {
             ans.push_back(nums);
+            return;
         }
-        while(next_permutation(nums.begin(),nums.end()));
         
+        for(int i = index; i < nums.size(); i++)
+        {
+            swap(nums[index], nums[i]);
+            permutation(nums, index + 1, ans);
+            swap(nums[index], nums[i]); //backtrack
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permutation(nums, 0, ans);
         return ans;
     }
 };
