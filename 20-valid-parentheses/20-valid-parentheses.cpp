6d9c36f751d31dae976f96956a296e523484b715
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-02 00:20:08 +0530

    Time: 4 ms (31.28%), Space: 6.4 MB (10.26%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {      
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(' or s[i]=='[' or s[i]=='{'){
+                st.push(s[i]);
+            }
+			
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char c = st.top();
+                st.pop();
+                if(s[i] == ')' and c == '('){
+                    continue;
+                }
+                else if(s[i] == '}' and c == '{'){
+                    continue;
+                }
+                else if(s[i] == ']' and c == '['){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        else
+            return false;
+    }
+};
