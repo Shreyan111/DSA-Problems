e6444528ce5219c7f13cc22e8862978c8c52309e
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-03 20:47:50 +0530

    Time: 37 ms (12.14%), Space: 19.8 MB (48.44%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    public:
+    // int trap(vector < int > & arr) {
+    //     int n = arr.size();
+    //     int waterTrapped = 0;
+    //     for (int i = 0; i < n; i++) {
+    //         int j = i;
+    //         int leftMax = 0, rightMax = 0;
+    //         while (j >= 0) {
+    //             leftMax = max(leftMax, arr[j]);
+    //             j--;
+    //         }
+    //         j = i;
+    //         while (j < n) {
+    //             rightMax = max(rightMax, arr[j]);
+    //             j++;
+    //         }
+    //         waterTrapped += min(leftMax, rightMax) - arr[i];
+    //     }
+    //     return waterTrapped;
+    // }
+    
+    int trap(vector < int > & height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int res = 0;
+        int maxLeft = 0, maxRight = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    res += maxLeft - height[left];
+                }
+                left++;
+            } 
+            
+            else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    res += maxRight - height[right];
+                }   
+                right--;
+            }
+        }
+        return res;
+    }
+};
