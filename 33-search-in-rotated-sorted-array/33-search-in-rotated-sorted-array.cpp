416f2dd918f3ddd0d3b8c3e461d01ff5b3e46003
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-03 09:48:01 +0530

    Time: 5 ms (59.84%), Space: 11.2 MB (29.10%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int low,
-                 int high, int key)
-{
-    if (high < low)
-        return -1;
- 
-    int mid = (low + high) / 2; 
-    if (key == nums[mid])
-        return mid;
- 
-    if (key > nums[mid])
-        return binarySearch(nums, (mid + 1), high, key);
- 
-    // else
-    return binarySearch(nums, low, (mid - 1), key);
-}
-    
-    int findPivot(vector<int>& nums, int low, int high){
-
-    if (high < low)
-        return -1;
-        
-    if (high == low)
-        return low;
- 
-    int mid = (low + high) / 2;
-    if (mid < high && nums[mid] > nums[mid + 1])
-        return mid;
- 
-    if (mid > low && nums[mid] < nums[mid - 1])
-        return (mid - 1);
- 
-    if (nums[low] >= nums[mid])
-        return findPivot(nums, low, mid - 1);
- 
-    return findPivot(nums, mid + 1, high);
-    }
-    
-    
-    int search(vector<int>& nums, int target) {
-        
+    int search(vector<int>& nums, int target) 
+    {
         int n = nums.size();
-        int pivot = findPivot(nums, 0, n - 1);
-        int key = target;
-        
-        if (pivot == -1)
-            return binarySearch(nums, 0, n - 1, key);
-        
-        if (nums[pivot] == key)
-            return pivot;
- 
-        if (nums[0] <= key)
-            return binarySearch(nums, 0, pivot - 1, key);
- 
-    return binarySearch(nums, pivot + 1, n - 1, key);
-        
+        int l = 0;
+        int r = n - 1;
+        while(r >= l)
+        {
+            int mid = l + (r-l)/2; //for finding mid in binary search
+            if(nums[mid] == target)
+                return mid;
+            
+            if(nums[l] <= nums[mid]) //to find if left part of array is sorted or not
+            {
+                if(target >= nums[l] && target <= nums[mid])
+                    r = mid - 1;
+                else
+                   l = mid + 1;          
+            }
+            
+            else
+            {
+                if(target >= nums[mid] && target <= nums[r])
+                    l = mid + 1; 
+                else
+                    r = mid - 1;
+            }     
+        }
+        return -1;
     }
 };
