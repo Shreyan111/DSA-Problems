305f4bee2bc3119e21958340f2d008791bf3d2f9
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-03 09:34:28 +0530

    Time: 56 ms (15.71%), Space: 27.3 MB (9.12%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int,int>map;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            map[nums[i]]++;
+        }
+        
+        for(auto x:map)
+        {
+            if(x.second==1){
+                return x.first;
+            }
+        }
+        return -1;
+    }
+};
