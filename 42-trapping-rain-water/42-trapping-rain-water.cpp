71305f7dca6e94e390c9366156babedb6564b4c9
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-12 10:29:03 +0530

    Time: 20 ms (70.51%), Space: 19.8 MB (55.19%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -25,7 +25,7 @@ class Solution {
         int left = 0, right = n - 1;
         int res = 0;
         int maxLeft = 0, maxRight = 0;
-        while (left <= right) {
+        while (left < right) {
             if (height[left] <= height[right]) {
                 if (height[left] >= maxLeft) {
                     maxLeft = height[left];
