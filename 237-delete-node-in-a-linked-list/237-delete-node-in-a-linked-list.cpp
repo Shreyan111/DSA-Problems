31c9c458c7c29b654be57553f8f25a73c122de42
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-07-18 14:56:53 +0530

    Time: 23 ms (32.27%), Space: 7.7 MB (39.02%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-    node -> val = node -> next -> val;
-    node -> next=node->next->next;  
+        node -> val = node -> next -> val; //allot value of 5 as 1 in example 1
+        node -> next = node -> next -> next;  //// connect new 1 with 9 and 5 got deleted and we broke link with previous 1
     }
 };
