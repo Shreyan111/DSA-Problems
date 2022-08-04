1e93bed384f78b9b0ab69aa65c19169591c125af
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-08-05 00:11:49 +0530

    Time: 147 ms (32.15%), Space: 27.8 MB (62.58%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -11,7 +11,7 @@ public:
         vector<int> ans(nums.size(), -1);
         for(int i = 0; i < nums.size(); i++)
         {
-            auto it = mp.lower_bound(nums[i][1]);
+            auto it = mp.lower_bound(nums[i][1]); //lower bound starts from 4 then, 3 and then 2 in example 2 and these lower bound are considered as key and accordingly the value is taken into consideration and changed with ans vector
             if(it != mp.end())
             {
                 ans[i] = it -> second;
