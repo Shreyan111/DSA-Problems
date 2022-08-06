33471a623d4e3511075b4c6bdadc318f1cc2ec5a
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-08-06 19:08:47 +0530

    Time: 85 ms (77.80%), Space: 19.8 MB (63.42%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> DIR = {
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {1, -1},
+        {0, -1},
+        {-1, -1},
+        {-1, 0},
+        {-1, 1}
+    };
+    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
+        int ans = 0;
+        int n = g.size();
+        int m = g[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m));
+        
+        if(g[0][0] || g[n - 1][m - 1]){
+            return -1;
+        }
+        
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        while(!q.empty()){
+            ans++;
+            int sz = q.size();
+            for(int i = 0; i < sz; i++){
+                auto temp = q.front();
+                q.pop();
+                int row = temp.first;
+                int col = temp.second;
+                if(row == n - 1 && col == m - 1) return ans;
+                for(int k = 0; k < 8; k++){
+                    int newR = row + DIR[k][0], newC = col + DIR[k][1];
+                    
+                    if(newR < 0 || newC < 0 || newR == n || newC == m || g[newR][newC] || vis[newR][newC])
+                        continue;
+                    q.push({newR, newC});
+                    vis[newR][newC] = true;
+                }
+            };
+        }
+        return -1;
+    }
+};
