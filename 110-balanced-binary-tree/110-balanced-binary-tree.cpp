e5dc737d74b99e4d1c1553c8bd22b5ae8e4f726f
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-04 13:58:03 +0530

    Time: 26 ms (32.62%), Space: 20.7 MB (94.96%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,29 +9,93 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         return dfsHeight(root) != -1; //if -1 is returned then false which means it is not balanced otherwise balanced
+//     }
+
+//     int dfsHeight (TreeNode *root) {
+//         if (root == NULL){
+//             return 0;
+//         }
+        
+//         int leftHeight = dfsHeight (root -> left);
+//         if (leftHeight == -1) 
+//             return -1;
+        
+//         int rightHeight = dfsHeight (root -> right);
+//         if (rightHeight == -1) 
+//             return -1;
+        
+//         if (abs(leftHeight - rightHeight) > 1)  
+//             return -1;
+
+//         return max(leftHeight, rightHeight) + 1;
+//     }
+// };
+
+
 class Solution {
 
 public:
     bool isBalanced(TreeNode* root) {
-        return dfsHeight(root) != -1; //if -1 is returned then false which means it is not balanced otherwise balanced
+        return dfsHeight(root) != -1;
     }
 
-    int dfsHeight (TreeNode *root) {
-        if (root == NULL){
+    int dfsHeight (TreeNode *node) {
+        if(node == NULL){
             return 0;
         }
         
-        int leftHeight = dfsHeight (root -> left);
-        if (leftHeight == -1) 
+        int lh = dfsHeight(node -> left);
+        if(lh == -1){
             return -1;
+        }
         
-        int rightHeight = dfsHeight (root -> right);
-        if (rightHeight == -1) 
+        int rh = dfsHeight(node -> right);
+        if(rh == -1){
             return -1;
+        }
         
-        if (abs(leftHeight - rightHeight) > 1)  
+        if(abs(lh - rh) > 1){
             return -1;
-
-        return max(leftHeight, rightHeight) + 1;
+        }
+        
+        return 1 + max(lh, rh);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
