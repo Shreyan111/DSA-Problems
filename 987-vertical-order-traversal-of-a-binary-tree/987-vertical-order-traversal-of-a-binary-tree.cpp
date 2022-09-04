09f93db4d8cd253fb8abfcf141a04ee42820562a
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-04 11:34:48 +0530

    Time: 17 ms (8.96%), Space: 13.5 MB (59.23%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,34 +9,76 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<vector<int>> verticalTraversal(TreeNode* root) {
+//         map<int, map<int, multiset<int>>> nodes;
+//         queue<pair<TreeNode* , pair<int, int>>> todo;
+//         todo.push({root,{0, 0}}); //initial vertical and level in queue
+//         while (!todo.empty()) {
+//             auto p = todo.front();
+//             todo.pop();
+//             TreeNode* temp = p.first;
+
+//             //x -> vertical , y->level
+//             //inorder traversal
+//             int x = p.second.first, y = p.second.second;
+//             nodes[x][y].insert(temp -> val); //inserting to multiset
+//             if (temp -> left) {
+//                 todo.push({temp -> left,{x - 1, y + 1}});
+//             }
+            
+//             if (temp -> right) {
+//             todo.push({temp -> right,{x + 1, y + 1}});
+//             }
+            
+//         }
+  
+//         vector<vector< int >> ans;
+//         //iterate each vertical
+//         for (auto p: nodes) {
+//             vector<int> col;
+//             //iterate the level and nodes in that vertical
+//             for (auto q: p.second) {
+//                 //insert nodes present in the level of each vertical
+//                 col.insert(col.end(), q.second.begin(), q.second.end());//multiple nodes can be also present in each level
+//             }
+//             ans.push_back(col);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode* , pair<int, int>>> todo;
-        todo.push({root,{0, 0}}); //initial vertical and level in queue
-        while (!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
+        map<int, map<int, multiset<int>>> maps;
+        queue<pair<TreeNode* , pair<int, int>>> stores;
+        stores.push({root, {0, 0}});
+        while (!stores.empty()) {
+            auto p = stores.front();
+            stores.pop();
             TreeNode* temp = p.first;
 
-            //x -> vertical , y->level
-            //inorder traversal
-            int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(temp -> val); //inserting to multiset
+            int x = p.second.first;
+            int y = p.second.second;
+            maps[x][y].insert(temp -> val);
             if (temp -> left) {
-                todo.push({temp -> left,{x - 1, y + 1}});
+                stores.push({temp -> left, {x - 1, y + 1}});
             }
             
             if (temp -> right) {
-            todo.push({temp -> right,{x + 1, y + 1}});
+                stores.push({temp -> right, {x + 1, y + 1}});
             }
             
         }
   
-        vector<vector< int >> ans;
+        vector<vector<int>> ans;
         //iterate each vertical
-        for (auto p: nodes) {
+        for (auto p: maps) {
             vector<int> col;
             //iterate the level and nodes in that vertical
             for (auto q: p.second) {
