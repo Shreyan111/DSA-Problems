de1aca3328c2e6640a4d5138528f53dfdc572f16
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-04 13:46:01 +0530

    Time: 30 ms (13.76%), Space: 20.1 MB (92.89%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -9,6 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int diameter = 0;
+//         height(root, diameter);
+//         return diameter;
+//     }
+
+// private:
+//     int height(TreeNode* node, int& diameter) {
+//         if (node == NULL) {
+//             return 0;
+//         }
+//         int lh = height(node -> left, diameter);
+//         int rh = height(node -> right, diameter);
+
+//         diameter = max(diameter, lh + rh);
+
+//         return 1 + max(lh, rh);
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
@@ -18,15 +43,35 @@ public:
     }
 
 private:
-    int height(TreeNode* node, int& diameter) {
-        if (node == NULL) {
+    int height(TreeNode* root, int& diameter) {
+        if(root == NULL){
             return 0;
         }
-        int lh = height(node -> left, diameter);
-        int rh = height(node -> right, diameter);
-
+        
+        int lh = height(root -> left, diameter);
+        int rh = height(root -> right, diameter);
+        
         diameter = max(diameter, lh + rh);
-
+        
         return 1 + max(lh, rh);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
