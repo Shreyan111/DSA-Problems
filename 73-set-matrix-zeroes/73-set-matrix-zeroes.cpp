88b78a95348ec04b9769fc695d1636d7c868c072
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-04 21:36:18 +0530

    Time: 21 ms (61.09%), Space: 13.2 MB (81.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,20 +1,84 @@
+// class Solution {
+// public:
+//     void setZeroes(vector < vector < int >> & matrix) {
+//         int rows = matrix.size(), cols = matrix[0].size();
+//         vector <int> dummy1(rows,-1), dummy2(cols,-1);
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     dummy1[i] = 0;
+//                     dummy2[j] = 0;
+//                 }
+//             }
+//         }
+        
+//         for (int i = 0; i < rows; i++) {
+//             for (int j = 0; j < cols; j++) {
+//                 if (dummy1[i] == 0 || dummy2[j]==0) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
 class Solution {
 public:
-    void setZeroes(vector < vector < int >> & matrix) {
-        int rows = matrix.size(), cols = matrix[0].size();
-        vector <int> dummy1(rows,-1), dummy2(cols,-1);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
+    void setZeroes(vector<vector<int>> & matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<int> dummy1(n, -1);
+        vector<int> dummy2(m, -1);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
                     dummy1[i] = 0;
                     dummy2[j] = 0;
                 }
             }
         }
         
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (dummy1[i] == 0 || dummy2[j]==0) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(dummy1[i] == 0 || dummy2[j] == 0){
                     matrix[i][j] = 0;
                 }
             }
