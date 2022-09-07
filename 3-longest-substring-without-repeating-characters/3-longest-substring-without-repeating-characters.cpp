e349e59ac829ec20b8365d01ac3e396f10c8c65c
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-07 17:13:53 +0530

    Time: 38 ms (39.75%), Space: 8.4 MB (60.39%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,56 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         map<char, int> ans;
+//         int maxLen = 0;
+//         int start = 0;
+//         int end;
+ 
+//         for(end = 0; end < s.length(); end++)
+//         {
+//             if (ans.find(s[end]) != ans.end())
+//             {
+//                 start = max(start, ans[s[end]] + 1);
+//             }
+            
+//             ans[s[end]] = end;
+//             maxLen = max(maxLen, end - start + 1);
+//         }
+        
+//         return maxLen;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char, int> ans;
+        map<int, int> maps;
         int maxLen = 0;
         int start = 0;
         int end;
- 
-        for(end = 0; end < s.length(); end++)
-        {
-            if (ans.find(s[end]) != ans.end())
-            {
-                start = max(start, ans[s[end]] + 1);
+        
+        for(int end = 0; end < s.length(); end++){
+            if(maps.find(s[end]) != maps.end()){
+                start = max(start, maps[s[end]] + 1);
             }
             
-            ans[s[end]] = end;
+            maps[s[end]] = end;
             maxLen = max(maxLen, end - start + 1);
         }
         
