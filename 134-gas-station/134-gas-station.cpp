b8f0cb24c79aff54bf3f6c3031b24d79f685a823
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-10 21:56:33 +0530

    Time: 211 ms (5.16%), Space: 69.4 MB (52.31%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,3 +1,59 @@
+// class Solution {
+// public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//         int sum = 0;
+//         int total_sum = 0;
+//         int ans = 0;
+        
+//         for(int i = 0; i < cost.size(); i++){
+//             total_sum = total_sum + (gas[i] - cost[i]);
+            
+//             sum = sum + (gas[i] - cost[i]);
+            
+//             if(sum < 0){
+//                 ans = i + 1;
+//                 sum = 0;
+//             }
+//         }
+//         if (total_sum >= 0){
+//             return ans;
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
@@ -6,16 +62,16 @@ public:
         int ans = 0;
         
         for(int i = 0; i < cost.size(); i++){
-            total_sum = total_sum + (gas[i] - cost[i]);
+            total_sum += (gas[i] - cost[i]);
             
-            sum = sum + (gas[i] - cost[i]);
+            sum += (gas[i] - cost[i]);
             
             if(sum < 0){
-                ans = i + 1;
                 sum = 0;
+                ans = i + 1;
             }
         }
-        if (total_sum >= 0){
+        if(total_sum >= 0){
             return ans;
         }
         return -1;
