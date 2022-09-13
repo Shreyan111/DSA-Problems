ad8492a95cb06755767faf227997aabc58cc7e55
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-13 11:40:24 +0530

    Time: 44 ms (18.15%), Space: 23.8 MB (30.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,12 +1,88 @@
+// class Solution {
+// public:
+//      int count=0;
+//      void helper(vector<vector<int>>& grid, vector<vector<bool>>& visited, int r, int c) {
+//         if(r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || visited[r][c] == true || grid[r][c] == 0){
+//             return;
+//         }
+//         visited[r][c] = true; 
+//         count++;
+//         helper(grid, visited, r - 1, c);
+//         helper(grid, visited, r, c - 1);
+//         helper(grid, visited, r + 1, c);
+//         helper(grid, visited, r, c + 1);
+//     }
+    
+    
+//     int maxAreaOfIsland(vector<vector<int>>& grid) {
+//         int row = grid.size();
+//         int col = grid[0].size();
+        
+//         vector<vector<bool>>visited(row, vector<bool>(col, false));
+//         int len = 0; //max area
+        
+//         for(int i = 0; i < row; i++){
+//             for(int j = 0; j < col; j++){
+//                 count = 0;
+//                 if(visited[i][j] == false && grid[i][j] == 1) {
+//                     helper(grid, visited, i, j );
+//                     len = max(count, len);
+//                 }
+//                 else {
+//                     visited[i][j] = true;
+//                 }
+//             }
+//         }
+//         return len;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-     int count=0;
+    int count; //declared globally so updates values globally in it
      void helper(vector<vector<int>>& grid, vector<vector<bool>>& visited, int r, int c) {
-        if(r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || visited[r][c] == true || grid[r][c] == 0){
+         if(r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || visited[r][c] == true || grid[r][c] == 0){
             return;
         }
         visited[r][c] = true; 
         count++;
+         
         helper(grid, visited, r - 1, c);
         helper(grid, visited, r, c - 1);
         helper(grid, visited, r + 1, c);
@@ -19,20 +95,21 @@ public:
         int col = grid[0].size();
         
         vector<vector<bool>>visited(row, vector<bool>(col, false));
-        int len = 0; //max area
+        int ans;
         
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 count = 0;
-                if(visited[i][j] == false && grid[i][j] == 1) {
-                    helper(grid, visited, i, j );
-                    len = max(count, len);
+                if(visited[i][j] == false && grid[i][j] == 1){
+                    helper(grid, visited, i, j);
+                    ans = max(ans, count);
                 }
-                else {
+                else{
                     visited[i][j] = true;
                 }
             }
         }
-        return len;
+        return ans;
     }
 };
+
