fa8bd323b9ff42c156e002ce61f793279833242c
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-17 10:44:32 +0530

    Time: 328 ms (17.76%), Space: 24.2 MB (19.13%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -6,22 +6,23 @@ public:
         }
 
         grid[i][j] = 2;
-        fill.push_back(make_pair(i,j));
+        fill.push_back(make_pair(i, j));
         flood_fill(grid, i + 1, j, fill, n, m);
         flood_fill(grid, i, j + 1, fill, n, m);
         flood_fill(grid, i - 1, j, fill, n, m);
         flood_fill(grid, i, j - 1, fill, n, m);
+        
         return;
     }
     int shortestBridge(vector<vector<int>>& grid) {
-        vector<pair<int,int>> island1;
-        vector<pair<int,int>> island2;
+        vector<pair<int, int>> island1;
+        vector<pair<int, int>> island2;
         int count = 0;
         int n = grid.size();
         int m = grid[0].size();
-        for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                if(grid[i][j]==1) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
                     count++;
                     if(count == 1) {
                         flood_fill(grid, i, j, island1, n, m);
@@ -38,7 +39,7 @@ public:
         int min_dist = INT_MAX;
         for(auto a : island1)
             for(auto b : island2) {
-                int dist = abs(a.first-b.first) + abs(a.second-b.second) - 1;
+                int dist = abs(a.first - b.first) + abs(a.second - b.second) - 1;
                 min_dist = min(min_dist, dist);
             }
         
