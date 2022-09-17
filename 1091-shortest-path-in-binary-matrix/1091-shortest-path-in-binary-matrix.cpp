308194c863857ff26b0882124208f180500902ca
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-17 12:44:23 +0530

    Time: 158 ms (34.49%), Space: 19.8 MB (62.59%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -16,7 +16,7 @@ public:
         int m = g[0].size();
         vector<vector<bool>> vis(n, vector<bool>(m));
         
-        if(g[0][0] || g[n - 1][m - 1]){
+        if(g[0][0] == 1 || g[n - 1][m - 1] == 1){
             return -1;
         }
         
@@ -33,14 +33,14 @@ public:
                 int col = temp.second;
                 if(row == n - 1 && col == m - 1) return ans;
                 for(int k = 0; k < 8; k++){
-                    int newR = row + DIR[k][0], newC = col + DIR[k][1];
+                    int newR = row + DIR[k][0];
+                    int newC = col + DIR[k][1];
                     
-                    if(newR < 0 || newC < 0 || newR == n || newC == m || g[newR][newC] || vis[newR][newC])
-                        continue;
+                    if(newR < 0 || newC < 0 || newR == n || newC == m || g[newR][newC] == 1 || vis[newR][newC]) continue;
                     q.push({newR, newC});
                     vis[newR][newC] = true;
                 }
-            };
+            }
         }
         return -1;
     }
