35357c9cd36aea03fa4a952cb42eefa60997b742
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-09-28 12:28:54 +0530

    Time: 9 ms (39.46%), Space: 10.7 MB (32.26%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -16,11 +16,11 @@ class Solution {
         ListNode* fast = start;
         ListNode* slow = start;
 
-        for(int i = 1; i <= n; ++i){
+        for(int i = 1; i <= n; i++){
             fast = fast -> next;
         }
     
-        while(fast ->next != NULL){
+        while(fast -> next != NULL){
             fast = fast -> next;
             slow = slow -> next;
         }
