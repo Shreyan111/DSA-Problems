d6333814ae948663852caee007b8b4d2d993dca5
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2022-12-05 10:25:58 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -4,25 +4,28 @@ using namespace std;
 
 
 // } Driver Code Ends
-
 class Solution {
   public:
+    int f(int ind, vector<int>& height, vector<int>& dp){
+        if(ind == 0) return 0;
+        
+        if(dp[ind] != -1) return dp[ind];
+        
+        int jump_two = INT_MAX;
+        int jump_one = f(ind - 1, height, dp) + abs(height[ind] - height[ind - 1]);
+        
+        if(ind > 1){
+            jump_two = f(ind - 2, height, dp) + abs(height[ind] - height[ind - 2]);
+        }
+        
+        return dp[ind] = min(jump_one, jump_two);
+    }
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
-        vector<int> dp(n, -1);
-        dp[0] = 0;
-        for(int ind = 1; ind < n; ind++){
-            int jumpTwo = INT_MAX;
-            int jumpOne= dp[ind - 1] + abs(height[ind] - height[ind-1]);
-            if(ind > 1)
-                jumpTwo = dp[ind - 2] + abs(height[ind] - height[ind-2]);
-                
-            dp[ind] = min(jumpOne, jumpTwo);
-        }
-        return dp[n - 1];
+        vector<int> dp(n + 1, -1);
+        return f(n - 1, height, dp);
     }
 };
-
 
 //{ Driver Code Starts.
 
