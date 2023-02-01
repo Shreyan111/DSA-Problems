60173831986d2741219e604c086d826b0e467121
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2023-02-01 14:17:26 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -1,32 +1,33 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
 
- // } Driver Code Ends
-class Solution{
-public:	
-    int f(int *arr, int ind, vector<int> &dp){
+// } Driver Code Ends
+class Solution
+{
+    public:
+    int find(int *arr, int ind, vector<int>& dp){
         if(ind == 0) return arr[ind];
         if(ind < 0) return 0;
         
         if(dp[ind] != -1) return dp[ind];
         
-        int pick = arr[ind] + f(arr, ind - 2, dp);
-        int not_pick = 0 + f(arr, ind - 1, dp);
+        int pick = arr[ind] + find(arr, ind - 2, dp);
+        int notPick = find(arr, ind - 1, dp);
         
-        return dp[ind] = max(pick, not_pick);
+        return dp[ind] = max(pick, notPick);
     }
-	// calculate the maximum sum with out adjacent
-	int FindMaxSum(int *arr, int n) {
-	    // code here
-	    vector<int> dp(n, -1);
-	    return f(arr, n - 1, dp);
-	}
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        vector<int> dp(n + 1, -1);
+        return find(arr, n - 1, dp);
+    }
 };
 
-
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main()
 {
     //taking total testcases
@@ -48,4 +49,5 @@ int main()
 	}
 	return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
