42b1b41e665c31879ac18ff10a4d0fe27c63d58d
Shreyan Sanyal <72288279+Shreyan111@users.noreply.github.com>
2023-02-01 19:42:38 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -3,32 +3,33 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int f(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp){
-        if(day == 0){
+    int f(int index, int last_task, vector<vector<int>> &points, vector<vector<int>> &dp){
+        if(index == 0){
             int maxi = 0;
             
             for(int task = 0; task < 3; task++){
-                if(task != last){
+                if(task != last_task){
                     maxi = max(maxi, points[0][task]);
                 }
             }
             return maxi;
         }
         
-        if(dp[day][last] != -1) return dp[day][last];
+        if(dp[index][last_task] != -1) return dp[index][last_task];
         
         int maxi = 0;
         
         for(int task = 0; task < 3; task++){
-            if(task != last){
-                int point = points[day][task] + f(day - 1, task, points, dp);
+            if(task != last_task){
+                int point = points[index][task] + f(index - 1, task, points, dp);
                 maxi = max(maxi, point);
             }
         }
         
-        return dp[day][last] = maxi;
+        return dp[index][last_task] = maxi;
     }
     
     int maximumPoints(vector<vector<int>>& points, int n) {
@@ -37,6 +38,7 @@ class Solution {
         return f(n - 1, 3, points, dp);
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
